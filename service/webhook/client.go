13634a8522ff32c0a7e96436c0053e1f6b6c0494
Nastassy Zagorov <nzagorov@onfleet.com>
2023-03-13 19:43:00 -0700

    webhook create and delete

--- service/webhook/client.go
+++ service/webhook/client.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	onfleet "github.com/onfleet/gonfleet"
+	"github.com/onfleet/gonfleet/util"
 )
 
 type caller func(apiKey string, httpClient *http.Client, method string, url string, body any, result any) error
@@ -28,4 +29,16 @@ func (c *Client) List() ([]onfleet.Webhook, error) {
 	webhooks := []onfleet.Webhook{}
 	err := c.call(c.apiKey, c.httpClient, http.MethodGet, c.url, nil, &webhooks)
 	return webhooks, err
+}
+
+func (c *Client) Create(params onfleet.WebhookCreateParams) (onfleet.Webhook, error) {
+	webhook := onfleet.Webhook{}
+	err := c.call(c.apiKey, c.httpClient, http.MethodPost, c.url, params, &webhook)
+	return webhook, err
+}
+
+func (c *Client) Delete(webhookId string) error {
+	url := util.UrlAttachPath(c.url, webhookId)
+	err := c.call(c.apiKey, c.httpClient, http.MethodDelete, url, nil, nil)
+	return err
 }
